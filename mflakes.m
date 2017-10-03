29ff7b3a962f30be783402ea3133ea66ce2b5902
Frey, Jedediah <jed-frey@users.noreply.github.com>
2017-10-03 19:22:13 -0400

    Created project script skeleton.

---
+++ mflakes.m
@@ -0,0 +1,28 @@
+% mflakes - A simple program which checks Matlab source files for errors.
+%
+% Syntax:
+%	[output1,output2] = mflakes(options)
+%
+% Inputs:
+%	input1 - InputDescription
+%	input2 - InputDescription
+%	input3 - InputDescription
+%
+% Outputs:
+%	output1 - OutputDescription
+%	output2 - OutputDescription
+%
+% Example:
+%	[output1,output2] = mflakes(directory, options)
+%	[output1,output2] = mflakes(options)
+%
+% See also: 
+
+% Author: Jed Frey
+% October 2017
+
+%------------- BEGIN CODE --------------
+
+
+
+%------------- END CODE ----------------
